d707013f166ac4b9565d4ebb36eae52849753202
bbarr <bbarr1384@gmail.com>
2013-08-26 17:20:53 -0400

    using env port if it is set

--- app/apis/index.js
+++ app/apis/index.js
@@ -1,6 +1,5 @@
 
-var factualApi = require('./factual.api');
-
-module.exports = [
-  factualApi
-]
+module.exports = require("fs")
+  .readdirSync(__dirname)
+  .filter(function(file) { console.log(file); return file.indexOf('.api') > -1; })
+  .map(function(file) { return require("./" + file); });
