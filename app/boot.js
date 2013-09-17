4a594259b3467cb31d0119907dafb7d7228f69b3
bbarr <bbarr1384@gmail.com>
2013-09-17 01:02:20 -0400

    logging

--- app/boot.js
+++ app/boot.js
@@ -5,5 +5,8 @@ var app = express();
 require('./middleware')(app);
 require('./routes')(app);
 
+app.use(express.logger());
+
 // and go!
+console.log('goooooo', process.env.PORT)
 app.listen(process.env.PORT || 3000);
