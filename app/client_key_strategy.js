dac1978feec5f9f3ac56fad57203821d886da0d8
bbarr <bbarr1384@gmail.com>
2013-09-17 01:13:44 -0400

    logging

--- app/client_key_strategy.js
+++ app/client_key_strategy.js
@@ -6,17 +6,21 @@ var clientLib = require('./client');
 
 function ClientKeyStrategy() { 
   this.name = 'client-key'; 
+  console.log(this.name)
 };
 
 util.inherits(ClientKeyStrategy, passport.Strategy);
 
 ClientKeyStrategy.prototype.authenticate = function(req, options) {
+  console.log('coooome on')
 
   var publicKey = req.param('publicKey');
   var authHash = req.param('authHash');
   var timestamp = req.param('timestamp');
 
-  clientLib.collection().then(function(clients) {
+  console.log('whaaaaaaaa')
+  clientLib.collection(function(clients) {
+    console.log('clients ', clients)
     clients.find({ publicKey: publicKey }).nextObject(function(e, client) {
       if (!client) return this.fail();
       if (!crypto.createHash('md5').update(timestamp, client.privateKey).digest('hex') === authHash) return this.fail();
@@ -25,5 +29,4 @@ ClientKeyStrategy.prototype.authenticate = function(req, options) {
     }.bind(this));
   }.bind(this));
 };
-
 module.exports = ClientKeyStrategy;
